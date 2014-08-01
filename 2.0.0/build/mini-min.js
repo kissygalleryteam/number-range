5debc953ffcc66119f4b542c50889d65a08aed4d
弘树 <tiehang.lth@alibaba-inc.com>
2014-08-01 13:28:03 +0800

    sync daily version 2.0.0

---
+++ 2.0.0/build/mini-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-05-05 9:31:58 AM*/
+KISSY.add("kg/number-range/2.0.0/mini",function(a,b){function c(){}var d=(b.all,a.Event.Target);return a.augment(c,d,{}),c},{requires:["node","lang"]});
